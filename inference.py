94fc8f968859feab8071625434f15b6897f8a35e
YonatanKoifman <yonatanko@campus.technion.ac.il>
2022-11-14 10:23:06 +0200

    Finished implementation of beam search (need to tun it)

--- inference.py
+++ inference.py
@@ -10,8 +10,9 @@ def memm_viterbi(sentence, pre_trained_weights, feature2id):
     Implement q efficiently (refer to conditional probability definition in MEMM slides)
     """
     n = len(sentence)
+    print(sentence)
     tags = feature2id.feature_statistics.tags
-    sentence_tags = [""] * len(sentence)
+    sentence_tags = [""] * (n-1)
     pi = {}
     bp = {}
     for i in range(n):
@@ -20,9 +21,9 @@ def memm_viterbi(sentence, pre_trained_weights, feature2id):
                 pi[(i, u, v)] = 0
                 bp[(i, u, v)] = ""
 
-    pi[(0, "*", "*")] = 1
+    pi[(1, "*", "*")] = 1
 
-    for k in range(1, n):
+    for k in range(2, n):
         for u in tags:
             for v in tags:
                 max_prob = -np.inf
@@ -45,11 +46,92 @@ def memm_viterbi(sentence, pre_trained_weights, feature2id):
                 max_num = pi[key]
                 max_u_v = (key[1], key[2])
 
-    (sentence_tags[n - 2], sentence_tags[n - 1]) = (max_u_v[0], max_u_v[1])
+    (sentence_tags[n - 3], sentence_tags[n - 2]) = (max_u_v[0], max_u_v[1])
 
-    for k in range(n - 3, 0, -1):
+    for k in range(n - 4, 0, -1):
         sentence_tags[k] = bp[(k + 2, sentence_tags[k + 1], sentence_tags[k + 2])]
-    return sentence_tags[1:]
+    return sentence_tags
+
+
+def init_pi_bp_beam_dict(n, tags):
+    pi = {}
+    bp = {}
+    beam_dict = {}
+    for i in range(n):
+        for u in tags:
+            for v in tags:
+                pi[(i, u, v)] = 0
+                bp[(i, u, v)] = ""
+
+    pi[(1, "*", "*")] = 1
+    beam_dict[1] = ["*"]
+    return pi, bp, beam_dict
+
+
+def memm_viterbi_with_beam_search(sentence, pre_trained_weights, feature2id):
+    """
+    Write your MEMM Viterbi implementation below
+    You can implement Beam Search to improve runtime
+    Implement q efficiently (refer to conditional probability definition in MEMM slides)
+    """
+    """
+        Write your MEMM Viterbi implementation below
+        You can implement Beam Search to improve runtime
+        Implement q efficiently (refer to conditional probability definition in MEMM slides)
+        """
+    n = len(sentence)
+    tags = feature2id.feature_statistics.tags
+    tags_pred = [""] * (n - 1)
+    pi, bp, beam_dict = init_pi_bp_beam_dict(n, tags)
+    beam_size = 2
+    for k in range(2, n):
+        if k == 2:
+            u = "*"
+            t = "*"
+            list_of_probs = []
+            for v in tags:
+                current = pi[(k - 1, t, u)] * q(v, t, u, pre_trained_weights, sentence,
+                                                    feature2id, k, tags)
+                pi[(k, u, v)] = current
+                bp[(k, u, v)] = t
+                list_of_probs.append((current, v))
+            max_probs_and_labels = sorted(list_of_probs)[-beam_size:]
+            beam_dict[k] = [x[1] for x in max_probs_and_labels]
+        else:
+            for u in beam_dict[k-1]:
+                for v in tags:
+                    max_prob = -np.inf
+                    arg_max_prob = ""
+                    for t in beam_dict[k-2]:
+                        current = pi[(k - 1, t, u)] * q(v, t, u, pre_trained_weights, sentence,
+                                                        feature2id, k, tags)
+                        if current > max_prob:
+                            max_prob = current
+                            arg_max_prob = t
+
+                    pi[(k, u, v)] = max_prob
+                    bp[(k, u, v)] = arg_max_prob
+
+            list_of_probs = []
+            for u in beam_dict[k-1]:
+                for v in tags:
+                    list_of_probs.append((pi[(k, u, v)], v))
+            max_probs_and_labels = sorted(list_of_probs)[-beam_size:]
+            beam_dict[k] = [x[1] for x in max_probs_and_labels]
+
+    max_u_v = ("", "")
+    max_num = -np.inf
+    for key in pi.keys():
+        if key[0] == n - 1:
+            if pi[key] > max_num:
+                max_num = pi[key]
+                max_u_v = (key[1], key[2])
+
+    (tags_pred[n - 3], tags_pred[n - 2]) = (max_u_v[0], max_u_v[1])
+
+    for k in range(n - 4, 0, -1):
+        tags_pred[k] = bp[(k + 2, tags_pred[k + 1], tags_pred[k + 2])]
+    return tags_pred
 
 
 def q(v, t, u, pre_trained_weights, sentence, feature2id, k, tags):
@@ -90,7 +172,7 @@ def tag_all_test(test_path, pre_trained_weights, feature2id, predictions_path):
     for k, sen in tqdm(enumerate(test), total=len(test)):
         sentence = sen[0]
         print("length of sentence: ", len(sentence))
-        pred = memm_viterbi(sentence, pre_trained_weights, feature2id)[1:]
+        pred = memm_viterbi_with_beam_search(sentence, pre_trained_weights, feature2id)[1:]
         sentence = sentence[2:]
         print("length of sentence: ", len(sentence))
         for i in range(len(pred)):
